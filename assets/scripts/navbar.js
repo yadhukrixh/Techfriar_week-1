5c66c0eaddd3e9588255f65619d2c0af869e260a
Yadhukrishnan <yadhukrishnan2030@gmail.com>
2024-08-11 14:19:15 +0530

    Updated scripts

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,11 +1,16 @@
+window.onload = () => {
+    let navbarToggle = document.querySelector('.menu-toggle');
+    let navLinks = document.querySelector('.nav-links');
 
-
-
-
-
-
-
-
+    if (navbarToggle && navLinks) {
+        navbarToggle.addEventListener('click', () => {
+            navLinks.classList.toggle('active');        
+            navbarToggle.classList.toggle('active');
+        });
+    } else {
+        console.error('Elements .menu-toggle or .nav-links not found');
+    }
+};
 
 
 
